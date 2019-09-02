aeb1132cf2ca8180e398f30b43c7fde7a96c7de7
Harry Sadler <harrysadlermusic@gmail.com>
2019-09-02 16:33:03 -0700

    moar learns

--- app/basic-types.go
+++ app/basic-types.go
@@ -9,17 +9,21 @@ import (
 
 // multiple variable declaration as "factored block"
 var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	b bool = false
+	s string = "hi there"
+	i int = -1234
+	f float32 = -1234.5678
+	MaxInt uint64 = 1<<64 - 1
+	z complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 
 func main() {
-
 	// print var types and values
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", b, b)
+	fmt.Printf("Type: %T Value: %v\n", s, s)
+	fmt.Printf("Type: %T Value: %v\n", i, i)
+	fmt.Printf("Type: %T Value: %v\n", f, f)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
-
 }
---
+++ app/type-conversions.go
@@ -0,0 +1,32 @@
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+
+	var x, y int = 3, 4
+
+	// cannot coerce by assignment
+	// var f float64 = math.Sqrt(x*x + y*y)
+	// var z uint = f
+
+	// must use explicit casting
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+
+	fmt.Println(x, y, z)
+
+
+	// more explicit casting examples
+	i := 64
+	j := string(i)
+	fmt.Println(j)
+
+
+}
+
