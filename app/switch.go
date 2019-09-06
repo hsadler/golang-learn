2d351fbce5ca6c455b7b1dc9af036dd5b212afc7
Harry Sadler <harrypatricksadler@gmail.com>
2019-09-06 10:26:47 -0700

    add another switch statement example

--- app/switch.go
+++ app/switch.go
@@ -4,9 +4,11 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
-func main() {
+
+func go_runs_on() {
 
 	fmt.Print("Go runs on ")
 
@@ -22,5 +24,30 @@ func main() {
 		fmt.Printf("%s.\n", os)
 	}
 
+}
+
+func when_is_saturday() {
+
+	fmt.Println("When's Saturday?")
+
+	// another switch statement example
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+}
+
+
+func main() {
+	go_runs_on()
+	when_is_saturday()
 }
 
