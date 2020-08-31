fb5dea9c5e3bb2f8cdc3e439faeca50785b238ac
Harry Sadler <harrysadlermusic@gmail.com>
2020-08-30 22:48:16 -0700

    add basic web server example code

--- run_web_app.sh
+++ run_web_app.sh
@@ -1,3 +1,3 @@
 
 # runs golang container with volume ./web_app
-docker run -it --rm -v $PWD/web_app:/go/src/web_app golang bash
+docker run -it --rm -v $PWD/web_app:/go/src/web_app -p 8090:8090 golang bash
