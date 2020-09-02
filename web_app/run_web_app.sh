b21c26a8dccbdd8e2ce16bfb5b87ecbc5ae84f0e
Harry Sadler <harrysadlermusic@gmail.com>
2020-09-02 22:34:00 +0300

    code cleanup, update TODOs

--- web_app/run_web_app.sh
+++ web_app/run_web_app.sh
@@ -1,7 +1,4 @@
 
-# runs golang container with volume ./web_app
-# docker run -it --rm -v $PWD:/go/src/web_app -p 8090:8090 golang bash
-
 # build docker container
 docker build --no-cache -f=Dockerfile -t=go_web_app:local .
 # run docker container with exposed port
