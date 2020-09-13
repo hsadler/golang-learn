2c42843a149525114fb3b7ab7c8bf947b19834b9
Harry Sadler <harrysadlermusic@gmail.com>
2020-09-13 18:21:09 +0200

    add hot reloading to web_app containerized golang server

--- web_app/run_web_app.sh
+++ web_app/run_web_app.sh
@@ -2,4 +2,8 @@
 # build docker container
 docker build --no-cache -f=Dockerfile -t=go_web_app:local .
 # run docker container with exposed port
-docker run -it --rm -p=8090:8090 --name=go_web_app go_web_app:local
+docker run -it --rm \
+    -v=$PWD:/go/src \
+    -p=8090:8090 \
+    --name=go_web_app go_web_app:local \
+
