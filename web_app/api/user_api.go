c3e54c8c1e7e361b09e47a6b9db57baaaa9fc606
Harry Sadler <harrysadlermusic@gmail.com>
2020-09-15 20:44:26 +0200

    restructure user model to have attached methods and api-formatted-data struct

--- web_app/api/user_api.go
+++ web_app/api/user_api.go
@@ -1,26 +1,17 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"web_app/model"
 	"web_app/service/jsonapi"
 )
 
-// GetUser : TODO add docstring
+// GetUser : endpoint for getting mock user data
 func GetUser(w http.ResponseWriter, req *http.Request) {
-	user := &model.User{Age: 1}
-	b, err := json.Marshal(user)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
+	user := &model.User{
+		FirstName: "Harry",
+		LastName:  "Sadler",
+		Age:       1,
 	}
-	success := true
-	userJSON := string(b)
-	jsonapi.SendJSONAPIResponse(
-		w,
-		success,
-		userJSON,
-	)
+	jsonapi.SendAPIResponse(w, true, user.GetAPIFormattedData())
 }
--- web_app/model/user.go
+++ web_app/model/user.go
@@ -1,6 +1,31 @@
 package model
 
-// User : TODO
+// User : user object
 type User struct {
-	Age int
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+// UserAPIFormattedData :
+type UserAPIFormattedData struct {
+	FirstName     string
+	LastName      string
+	FormattedName string
+	Age           int
+}
+
+// GetFormattedName :
+func (u User) GetFormattedName() string {
+	return u.FirstName + " " + u.LastName
+}
+
+// GetAPIFormattedData :
+func (u User) GetAPIFormattedData() *UserAPIFormattedData {
+	return &UserAPIFormattedData{
+		FirstName:     u.FirstName,
+		LastName:      u.LastName,
+		FormattedName: u.GetFormattedName(),
+		Age:           u.Age,
+	}
 }
